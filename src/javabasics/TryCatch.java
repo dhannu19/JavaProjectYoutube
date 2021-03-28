956544b9db48106d357833ec1b10eb63ab289df5
Prakash <prakashmkabadi@gmail.com>
2021-03-27 22:49:37 -0500

    Try catch code

---
+++ src/javabasics/TryCatch.java
@@ -0,0 +1,19 @@
+package javabasics;
+
+public class TryCatch {
+
+	public static void main(String[] args) {
+		try {
+		int a[] = {6,7,8,9,10,3};
+		
+		System.out.println("Array value :" +a[6]);
+		   }catch(Exception e)
+		{
+			   System.out.println(e);
+		}
+		
+		
+
+	}
+
+}
